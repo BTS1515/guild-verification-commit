38d9e47cd75d0a75d4f156ba2bcc69ce98793fd0
BTS1515 <Krabii56@outlook.com>
2026-03-08 20:44:44 +0300

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T17:44:07.556Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-08T17:44:44.563Z
+Commit: Add export functionality for reports
