22b2e0089e62f6d4f7c3a72462601e159be1ea1e
BTS1515 <Krabii56@outlook.com>
2026-03-08 20:45:57 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T17:45:56.132Z
-Commit: Add multi-language support
+Timestamp: 2026-03-08T17:45:58.197Z
+Commit: Resolve timezone issues in date handling
