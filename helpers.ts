f5d7a5eb2523001b4f6e385c64b4c8cdd5345f92
BTS1515 <Krabii56@outlook.com>
2026-03-08 20:46:36 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T17:46:16.849Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-08T17:46:37.249Z
+Commit: Fix CSS styling bug on mobile devices
