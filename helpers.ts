be5e78252d33bf103002335c4606d027151ddf48
BTS1515 <Krabii56@outlook.com>
2026-03-08 20:46:54 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T17:46:41.359Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-08T17:46:54.981Z
+Commit: Fix race condition in async operations
