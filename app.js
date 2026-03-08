3f4e5cc96db7a555d67d48cdc7dbef13b667e120
BTS1515 <Krabii56@outlook.com>
2026-03-08 20:45:23 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T17:45:07.280Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-08T17:45:23.768Z
+Commit: Fix memory leak in event handlers
