aff26291ba5c8d29d2a2db63cf24b3565d95a832
BTS1515 <Krabii56@outlook.com>
2026-03-08 20:43:42 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T17:43:37.252Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-08T17:43:43.312Z
+Commit: Fix broken links in documentation
