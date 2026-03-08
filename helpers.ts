030d15a4a76097c80efcd8b80ba29875b2c5cdaf
BTS1515 <Krabii56@outlook.com>
2026-03-08 20:44:56 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T17:44:44.563Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-08T17:44:56.960Z
+Commit: Resolve null pointer exception in user service
