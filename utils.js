64d7e27fa4c165564ee3372b4e9fa67d3d6ed24f
BTS1515 <Krabii56@outlook.com>
2026-03-08 20:46:26 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T17:46:18.704Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-08T17:46:26.510Z
+Commit: Fix broken links in documentation
