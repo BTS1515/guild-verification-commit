e57348796098f55e4abc5b90d634dceef4608276
BTS1515 <Krabii56@outlook.com>
2026-03-08 20:46:30 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T17:46:26.510Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-08T17:46:31.231Z
+Commit: Fix memory leak in event handlers
