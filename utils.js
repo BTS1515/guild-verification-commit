7fe6f750c72ac2195dd11a7ad71c46db35172d46
BTS1515 <Krabii56@outlook.com>
2026-03-08 20:43:51 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T17:43:45.216Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-08T17:43:52.209Z
+Commit: Patch API endpoint error handling
